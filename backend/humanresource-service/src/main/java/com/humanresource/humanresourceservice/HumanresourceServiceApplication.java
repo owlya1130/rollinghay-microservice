f7ce81f363f319ab448e71a0d441809b4ccc7709
凌政楠 Sam Ling <sam.ling@tpisoftware.com>
2023-09-03 09:10:36 +0000

    create 2 repos

---
+++ backend/humanresource-service/src/main/java/com/humanresource/humanresourceservice/HumanresourceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.humanresource.humanresourceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanresourceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanresourceServiceApplication.class, args);
+	}
+
+}
---
+++ backend/humanresource-service/src/test/java/com/humanresource/humanresourceservice/HumanresourceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humanresource.humanresourceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanresourceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
